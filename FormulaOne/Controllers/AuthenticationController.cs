3beae494b6f0a7c24fd7cb7f225fcfa428462541
Touseef90 <touseef.ghazanfar@gmail.com>
2024-08-11 15:47:09 +0500

    Adding Authentication Controller

---
+++ FormulaOne/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using FormulaOne.Configurations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager, JwtConfig jwtConfig)
+        {
+            _userManager = userManager;
+            _jwtConfig = jwtConfig;
+        }
+    }
+}
