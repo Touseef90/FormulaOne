1faac03fb22e29e217be924a75e6765c0f1c3565
Touseef90 <touseef.ghazanfar@gmail.com>
2024-08-11 16:58:00 +0500

    Adding registration action

--- FormulaOne/Controllers/AuthenticationController.cs
+++ FormulaOne/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 ﻿using FormulaOne.Configurations;
+using FormulaOne.Models;
+using FormulaOne.Models.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +17,55 @@ namespace FormulaOne.Controllers
         {
             _userManager = userManager;
             _jwtConfig = jwtConfig;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto requestDto)
+        {
+            // Validate incoming request
+            if (ModelState.IsValid)
+            {
+                // We need to check if email already exist
+                var user_exist = await _userManager.FindByEmailAsync(requestDto.Email);
+
+                if (user_exist != null)
+                {
+                    return BadRequest(new AuthResults()
+                    {
+                        Result = false,
+                        Errors = new List<string>()
+                        {
+                            "Email already exist"
+                        }
+                    });
+                }
+
+                // Create a user
+                var new_user = new IdentityUser()
+                {
+                    Email = requestDto.Email,
+                    UserName = requestDto.Email
+                };
+
+                var is_created = await _userManager.CreateAsync(new_user, requestDto.Password);
+
+                if (is_created.Succeeded)
+                {
+                    // Generate token
+                }
+
+                return BadRequest(new AuthResults()
+                {
+                    Result = false,
+                    Errors = new List<string>()
+                    {
+                        "Server error"
+                    }
+                });
+            }
+
+            return BadRequest();
         }
     }
 }
