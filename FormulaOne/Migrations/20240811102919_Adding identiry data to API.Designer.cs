9de405d0a53981cd06e3c43ff37db240ab8ecf37
Touseef90 <touseef.ghazanfar@gmail.com>
2024-08-11 15:34:21 +0500

    Adding Identity Db Context 'IdentityDbContext'

--- FormulaOne/Migrations/20240811102919_Adding identiry data to API.Designer.cs
+++ FormulaOne/Migrations/20240811102919_Adding identiry data to API.Designer.cs
@@ -4,6 +4,7 @@ using FormulaOne.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FormulaOne.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240811102919_Adding identiry data to API")]
+    partial class AddingidentirydatatoAPI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
